d1fa2dbe1469ca6eecec1bb0ab486fe2e607d560
shaw2024 <167036376+shaw2024@users.noreply.github.com>
2025-12-17 15:36:42 +0000

    Updates

--- server/models/LikePG.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const LikeSchema = new mongoose.Schema({
-  dogId: { type: mongoose.Schema.Types.ObjectId, required: true },
-  targetDogId: { type: mongoose.Schema.Types.ObjectId, required: true }
-}, { timestamps: true });
-
-const Like = mongoose.model('Like', LikeSchema);
-export default Like;
--- server/models/MatchPG.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const MatchSchema = new mongoose.Schema({
-  dog1Id: { type: mongoose.Schema.Types.ObjectId, required: true },
-  dog2Id: { type: mongoose.Schema.Types.ObjectId, required: true }
-}, { timestamps: true });
-
-const Match = mongoose.model('Match', MatchSchema);
-export default Match;
